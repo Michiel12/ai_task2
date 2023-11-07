bbd1ed1fe14df05821fd4174bbeef02e9ffdfc1d
Michiel Van Loy <91117399+Michiel12@users.noreply.github.com>
2023-11-07 23:27:49 +0100

    Update task_ml_michiel_van_loy.py

--- task_ml_michiel_van_loy.py
+++ task_ml_michiel_van_loy.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 import category_encoders as ce
 from sklearn import metrics
 
-st.title('AI task 2 by Michiel Van Loy r0889624')
+st.title('AI task 2 by Michiel Van Loy')
 st.header("Diabetes Prediction App")
 
 # read and split data
@@ -41,8 +41,8 @@ if method == "Decision Tree Classifier":
     st.header("Decision Tree Classifier")
     st.subheader("Explanation")
     st.write("The Decision Tree Classifier begins with selecting the features. These features will be used to ask questions. These questions are for example about the age, is the person older or younger than 30 years, and these question will form a tree. This will continue until it goes through all features or until a maximum depth is met. The values of the prediction will go through the tree to get a prediction.")
-    st.write("As you can see, this algorithm has an accuracy of around 0.75 - 0.85. Also, the decision matrix shows that this algorithm sometimes has a lot of false positive values, this means that the prediction was positive when it should have been negative.")
     st.subheader("Accuracy")
+    st.write("As you can see, this algorithm has an accuracy of around 0.75 - 0.85. Also, the decision matrix shows that this algorithm sometimes has a lot of false positive values, this means that the prediction was positive when it should have been negative.")
     st.write("Accuracy:", accuracy)
     st.write("Confusion Matrix:")
     st.write("[[True Negative  False Positive]")
@@ -60,8 +60,9 @@ elif method == "Support Vector Machine":
 
     st.subheader("Explanation")
     st.write("In Linear Support Vector Machine, you start off with for example two categories, cats and dogs. Within this categories SVM will try to find a line to split the data into two groups. This line is called the decision boundary. This line will be based on special points, these points are called support vectors and they make sure the line takes the best route. The line will try to have as much distance between the support vectors, it wants the largest margin. Like expected will SVM first have to train itself but afterwards it can tell the difference between both groups.")
-    st.write("As you can see, the accuracy of SVM is not as good, but it is a lot more consistent. When we look at the confusion matrix, you can see that also this algorithm has a lot of false positive values. Luckily it is better to have false positives than false negatives.")
     st.subheader("Accuracy")
+    st.write("As you can see, the accuracy of SVM is not as good, but it is a lot more consistent. When we look at the confusion matrix, you can see that also this algorithm has a lot of false positive values. Luckily it is better to have false positives than false negatives.")
+    
     st.write("Accuracy:", accuracy)
     st.write("Confusion Matrix:")
     st.write("[[True Negative  False Positive]")
@@ -83,8 +84,8 @@ elif method == "K-Nearest Neighbors":
 
     st.subheader("Explanation")
     st.write("Also for K-Nearest Neighbors we have data that we want to split in two group, red and blue. These colors will be represented on a map, where each house will have a color. When KNN wants to predict the color of a new house it will look at his neighbors. The amount of neighbors KNN will look at (in my case 4), will decide what color the new house will get. It will choose the color that occurs the most in the neighbors. The amount of neighbors to look at can be different for each scenario, here are some of the number with their accuracy: *1 = 0.81*, *2 = 0.88*, *3 = 0.85*, *4 = 0.89* and *5 = 0.88*. In my case, 4 is the ideal amount of neighbors to look at.")
-    st.write("Here you can see that KNN has the best accuracy. This is also because I can choose the amount of neighbors to look at myself so I can choose the best number. Also the confusion matrix has the best output of them all, it has the most right answers, the only downside is that KNN has the highest amount of false negative values.")
     st.subheader("Accuracy")
+    st.write("Here you can see that KNN has the best accuracy. This is also because I can choose the amount of neighbors to look at myself so I can choose the best number. Also the confusion matrix has the best output of them all, it has the most right answers, the only downside is that KNN has the highest amount of false negative values.")
     st.write("Accuracy:", accuracy)
     st.write("Confusion Matrix:")
     st.write("[[True Negative  False Positive]")
