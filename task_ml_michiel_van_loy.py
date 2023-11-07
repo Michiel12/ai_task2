09d910959809876788a98963ad326711165fd48c
Michiel Van Loy <91117399+Michiel12@users.noreply.github.com>
2023-11-07 20:59:21 +0100

    Update task_ml_michiel_van_loy.py

--- task_ml_michiel_van_loy.py
+++ task_ml_michiel_van_loy.py
@@ -39,8 +39,8 @@ if method == "Decision Tree Classifier":
     
     st.write("Accuracy:", accuracy)
     st.write("Confusion Matrix:")
-    st.write("[[True Negative (TN)  False Positive (FP)]")
-    st.write(" [False Negative (FN)  True Positive (TP)]]")
+    st.write("[[True Negative  False Positive]")
+    st.write(" [False Negative  True Positive]]")
     st.write(confusion)
     st.write("The Decision Tree Classifier begins with selecting the features. These features will be used to ask questions. These question are for example about the age, is the person older or younger then 30 years, and these question will form a tree. This will continue until it goes trough all features or until a maximum depth is met. The values of the prediction will go trough the tree to get a prediction.")
 
@@ -55,15 +55,19 @@ elif method == "Support Vector Machine":
 
     st.write("Accuracy:", accuracy)
     st.write("Confusion Matrix:")
-    st.write("[[True Negative (TN)  False Positive (FP)]")
-    st.write(" [False Negative (FN)  True Positive (TP)]]")
+    st.write("[[True Negative  False Positive]")
+    st.write(" [False Negative  True Positive]]")
     st.write(confusion)
     st.write("In Linear Support Vector Machine you start of with for exmaple two categories, cats and dogs. In this categorie SVM will try to find a line to plit the data into two groups. This line is called the decision boundry. This line will be based on special points, these points are called support vectors and they make sure the line takes the best route. The line will try to have as much distance between the support vectors, it wants the largest margin. Like expected will SVM first have to train itself but afterwards it can tell the difference between both groups.")
 
 elif method == "K-Nearest Neighbors":
     # K-Nearest Neighbors
     from sklearn.neighbors import KNeighborsClassifier
-    clf = KNeighborsClassifier(n_neighbors=4)
+    
+    # Dropdown for selecting the number of neighbors
+    k_neighbors = st.selectbox("Select the number of neighbors (K):", list(range(1, 11)))
+
+    clf = KNeighborsClassifier(n_neighbors=k_neighbors)
     clf.fit(X_cat_oh, y)
     prediction = clf.predict(X_cat_oh_test)
     accuracy = metrics.accuracy_score(y_test, prediction)
@@ -71,7 +75,7 @@ elif method == "K-Nearest Neighbors":
 
     st.write("Accuracy:", accuracy)
     st.write("Confusion Matrix:")
-    st.write("[[True Negative (TN)  False Positive (FP)]")
-    st.write(" [False Negative (FN)  True Positive (TP)]]")
+    st.write("[[True Negative  False Positive]")
+    st.write(" [False Negative  True Positive]]")
     st.write(confusion)
     st.write("For K-Nearest Neighbors we have data than we want to plit in two group, red and blue. These colors will be represented on a map where each house will have a color. When KNN wants to predict the color of a new house it will look at his neighbors. The amount of neighbors KNN will look at (in my casue 3), will decide what color the new house will get. It will choose the color that occurs the most in the neighbors. The amount of neighbors to look at can be different for each cenario, here are some of the number with their accuracy: *1 = 0.81*, *2 = 0.88*, *3 = 0.85*, *4 = 0.89* and *5 = 0.88*. In my case 4 is the ideal amount of neighbors to look at.")
